67f8be387507d3534203f1f56346a379236be1fd
m-blue <m-blue@users.noreply.github.com>
2016-09-01 13:13:01 -0500

    Add files via upload

---
+++ Madlib.py
@@ -0,0 +1,27 @@
+# variables
+animal1 = "cow"
+animal2 = "snake"
+animal3 = "cat"
+animal4 = "kangaroo"
+animal5 = "alpaca"
+adjective1 = "smelly"
+adjective2 = "tall"
+adjective3 = "groovy"
+adjective4 = "sloppy"
+adjective5 = "vulgar"
+location = "Starbucks"
+food = "jelly beans"
+
+# Nursery Rhyme
+print("This " + adjective1 + " " + animal1 + " went to " + location + ",\n")
+print("This " + adjective2 + " " + animal2 + " stayed home,\n")
+print("This " + adjective3 + " " + animal3 + " had " + food + ",\n")
+print("This " + adjective4 + " " + animal4 + " had none,\n")
+print("This " + adjective5 + " " + animal5 + " cried wee wee wee all the way home")
+
+#Original Rhyme:
+#This little piggy went to market,
+#This little piggy stayed home,
+#This little piggy had roast beef
+#This little piggy had none,
+#And this little piggy cried wee wee wee all the way home
